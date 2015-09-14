e9693d4ecfe0dbc23b29905d0f1f457f1b8a3afd
Howard.Zuo <leftstick@qq.com>
2015-09-14 19:04:26 +0800

    update demo

--- demo.js
+++ demo.js
@@ -1,9 +1,15 @@
 (function(angular) {
     'use strict';
 
-    var demo = angular.module('demo', ['ngAnimate', 'mgcrea.ngStrap', 'hljs', 'angular-theme-spinner']);
+    var demo = angular.module('demo', [
+        'ngAnimate',
+        'mgcrea.ngStrap',
+        'hljs',
+        'angular-theme-spinner'
+    ]);
 
-    demo.config(['hljsServiceProvider',
+    demo.config([
+        'hljsServiceProvider',
         function(hljsServiceProvider) {
             hljsServiceProvider.setOptions({
                 tabReplace: '    '
@@ -11,69 +17,83 @@
         }
     ]);
 
-    demo.directive('backToTop', ['$document', function($document) {
-        return {
-            restrict: 'A',
-            link: function(scope, element) {
+    demo.directive('backToTop', [
+        '$document',
+        function($document) {
+            return {
+                restrict: 'A',
+                link: function(scope, element) {
 
-                element.on('click', function(e) {
-                    $document.find('body').animate({
-                        scrollTop: 0
+                    element.on('click', function(e) {
+                        $document.find('body').animate({
+                            scrollTop: 0
+                        });
+                        e.preventDefault();
+                        e.stopPropagation();
                     });
-                    e.preventDefault();
-                    e.stopPropagation();
-                });
 
-                scope.$on('$destroy', function() {
-                    element.off('click');
-                });
-            }
-        };
-    }]);
+                    scope.$on('$destroy', function() {
+                        element.off('click');
+                    });
+                }
+            };
+        }
+    ]);
 
-    demo.controller('ThemeController', ['$scope',
+    demo.controller('ThemeController', [
+        '$scope',
         function($scope) {
 
-            $scope.themes1 = [{
-                name: 'tailing',
-                source: '<th-spinner theme="\'tailing\'" display="\'true\'" size="\'size-md\'"></th-spinner>',
-                size: 'size-md',
-                running: true
-            }, {
-                name: 'audio-wave',
-                source: '<th-spinner theme="\'audio-wave\'" display="\'true\'" size="\'size-md\'"></th-spinner>',
-                size: 'size-md',
-                running: true
-            }, {
-                name: 'windcatcher',
-                source: '<th-spinner theme="\'windcatcher\'" display="\'true\'" size="\'size-md\'"></th-spinner>',
-                size: 'size-md',
-                running: true
-            }];
+            $scope.themes1 = [
+                {
+                    name: 'tailing',
+                    source: '<th-spinner theme="\'tailing\'" display="\'true\'" size="\'size-md\'"></th-spinner>',
+                    size: 'size-md',
+                    running: true
+                },
+                {
+                    name: 'audio-wave',
+                    source: '<th-spinner theme="\'audio-wave\'" display="\'true\'" size="\'size-md\'"></th-spinner>',
+                    size: 'size-md',
+                    running: true
+                },
+                {
+                    name: 'windcatcher',
+                    source: '<th-spinner theme="\'windcatcher\'" display="\'true\'" size="\'size-md\'"></th-spinner>',
+                    size: 'size-md',
+                    running: true
+                }
+            ];
 
-            $scope.themes2 = [{
-                name: 'spinner-section',
-                source: '<th-spinner theme="\'spinner-section\'" display="\'true\'" size="\'size-md\'"></th-spinner>',
-                size: 'size-md',
-                running: true
-            }, {
-                name: 'spinner-section-far',
-                source: '<th-spinner theme="\'spinner-section-far\'" display="\'true\'" size="\'size-md\'"></th-spinner>',
-                size: 'size-md',
-                running: true
-            }, {
-                name: 'circular',
-                source: '<th-spinner theme="\'circular\'" display="\'true\'" size="\'size-md\'"></th-spinner>',
-                size: 'size-md',
-                running: true
-            }];
+            $scope.themes2 = [
+                {
+                    name: 'spinner-section',
+                    source: '<th-spinner theme="\'spinner-section\'" display="\'true\'" size="\'size-md\'"></th-spinner>',
+                    size: 'size-md',
+                    running: true
+                },
+                {
+                    name: 'spinner-section-far',
+                    source: '<th-spinner theme="\'spinner-section-far\'" display="\'true\'" size="\'size-md\'"></th-spinner>',
+                    size: 'size-md',
+                    running: true
+                },
+                {
+                    name: 'circular',
+                    source: '<th-spinner theme="\'circular\'" display="\'true\'" size="\'size-md\'"></th-spinner>',
+                    size: 'size-md',
+                    running: true
+                }
+            ];
 
-            $scope.themes3 = [{
-                name: 'initspin',
-                source: '<th-spinner theme="\'initspin\'" display="\'true\'" size="\'size-md\'"></th-spinner>',
-                size: 'size-md',
-                running: true
-            }];
+            $scope.themes3 = [
+                {
+                    name: 'initspin',
+                    source: '<th-spinner theme="\'initspin\'" display="\'true\'" size="\'size-md\'"></th-spinner>',
+                    size: 'size-md',
+                    running: true
+                }
+            ];
 
 
 
